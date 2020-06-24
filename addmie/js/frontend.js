62461ad9811b59c630e6cdb9a21356e922f2028d
codewithnick <nikhilsingh526452@gmail.com>
2020-06-24 15:55:23 +0530

    likecommentsharesave

---
+++ addmie/js/frontend.js
@@ -0,0 +1,20 @@
+function like(id){
+  const element=document.getElementById('like'+id);
+  element.classList.toggle('fas')
+  element.classList.toggle('far')
+  element.classList.toggle('red')
+}
+function comment(id){
+  const element=document.getElementById('comment'+id);
+  
+}
+function share(id){
+  const element=document.getElementById('share'+id);
+
+}
+function save(id){
+  const element=document.getElementById('save'+id);
+  element.classList.toggle('fas')
+  element.classList.toggle('far')
+  element.classList.toggle('gold')
+}
