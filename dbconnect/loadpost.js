7d0f5d73a6ddea12f7adfcc64b833827011c719f
codewithnick <nikhilsingh526452@gmail.com>
2020-07-08 18:56:17 +0530

    comments adde complete;\ly

--- dbconnect/loadpost.js
+++ dbconnect/loadpost.js
@@ -2,14 +2,15 @@ module.exports=async function main(req,res,client){
     
    var mongodb=require('mongodb')
     try {
-        var result=await client.db('profile').collection('post').findOne({_id:mongodb.ObjectId(req.query.id)})
-        //console.log(result)
+        var posts=await client.db('profile').collection('post').findOne({_id:mongodb.ObjectId(req.query.id)});
+        var comments=await client.db('profile').collection('comment').find({postid:req.query.id}).toArray();
+        console.log(posts,comments)
     } catch (e) {
         console.log('error connecting to db');
         console.error(e);
     } finally {
      
-        res.render('post.ejs',{username:req.session.username,postobj:result})
+        res.render('post.ejs',{username:req.session.username,postobj:posts,comments:comments})
     }
 
     
